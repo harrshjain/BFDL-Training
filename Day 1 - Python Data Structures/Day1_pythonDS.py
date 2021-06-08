d53fcc3173494a033f6b5c225d7d61e41d2d0f6e
Harsh Jain <harshjacksn@gmail.com>
2021-06-08 08:42:22 +0530

    Add files via upload

---
+++ Day 1 - Python Data Structures/Day1_pythonDS.py
@@ -0,0 +1,33 @@
+d = {'a' : 'apple', 'b' : 'banana', 'c' : {'d' : 'dog', 'e' : 'eggs', 'f' : {'g' : 'gun'}}}
+
+def getValues(d, lst):
+    for x, y in d.items():
+        if type(d[x]) is dict:
+            getValues(d[x], lst)
+        else:
+            lst.append(y)
+    return lst
+
+def getOutput(d,key):
+    lst = []
+    for a, b in d.items():
+        if a == key:
+            if type(d[a]) is dict:
+                return getValues(d[a], lst)
+            else:
+                return b
+        if type(d[a]) is dict:
+            return getOutput(d[a], key)
+
+def get(d, key):
+    temp = getOutput(d, key)
+    if type(temp) is list:
+        print(*temp)
+    else:
+        print(temp)
+
+        
+get(d,'c')
+print(getOutput(d,'f'))
+print(getOutput(d,'b'))
+print(getOutput(d,'e'))
